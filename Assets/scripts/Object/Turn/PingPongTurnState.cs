bd868c14b2992a8c97e42cbc5da2718cad46618c
unknown <plagueddog@joycity.com>
2019-01-15 18:49:02 +0900

    TEST

---
+++ Assets/scripts/Object/Turn/PingPongTurnState.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UniRx;
+using UnityEngine;
+
+public class PingPongTurnState {
+
+    public enum ePingPongTurnState
+    {
+        MyTurn,
+        EnemyTurn,
+    }
+    private ReactiveProperty<ePingPongTurnState> curState = null;
+    private IObservable<ePingPongTurnState> obchanged = null;
+    public IObservable<ePingPongTurnState> ObChanged
+    {
+        get
+        {
+            return obchanged;
+        }
+    }
+
+    public PingPongTurnState()
+    {
+        curState = new ReactiveProperty<ePingPongTurnState>();
+        obchanged = curState.DistinctUntilChanged();
+    }
+
+
+
+    public void MyTurnEnd()
+    {
+        curState.Value = ePingPongTurnState.EnemyTurn;
+    }
+    public void EnemyTurnEnd()
+    {
+        curState.Value = ePingPongTurnState.MyTurn;
+    }
+
+}
